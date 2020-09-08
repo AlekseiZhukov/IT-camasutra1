f43d92555beb99c2d4f9f1bffdd36f1d35e7cb33
AlekseiZhukov <alex__zhukov@mail.ru>
2020-09-08 19:28:48 +0300

    kabzda first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://fr.freelogodesign.org/Content/img/logo-ex-7.png" />
+    </header>
+}
+
+
+export default Header
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content' >
+            <div>
+                <img src='https://jssors8.azureedge.net/demos/image-slider/img/faded-monaco-scenery-evening-dark-picjumbo-com-image.jpg' />
+            </div>
+            <div className="ava-description" >
+                <img src='https://fsb.zobj.net/crop.php?r=5a-j9iwed47sGHVmkzCYJsZUP5ni5bN-ZPRcaygQQKH1iT8MxbxDatSYsuDSRoWeQzoqkOBh40NdDtOhMM0Y47TiotH3MDRn0OjaUapG2dNi7Emt3lIPPaaLCOMifwX6cF_NHKFeZFadoTvh' />
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div >
+    )
+}
+
+
+export default Profile
